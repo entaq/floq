c17cfbe4d482638957cd578a35111ebf849571a0
Arun Nagarajan <Arun Nagarajan>
2017-03-26 20:20:39 -0400

    added initial ios files

---
+++ Floq/Floq/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Floq
+//
+//  Created by Arun Nagarajan on 3/26/17.
+//  Copyright © 2017 Arun Nagarajan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
