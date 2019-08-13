70b18cfbbdf9b42c8a39de47f468ec9098e9479e
Codesworth <codezworth@gmail.com>
2019-08-13 18:29:57 +0000

    Added Assets

---
+++ Floq/Floq/App/Subscriptions/Alerts.swift
@@ -0,0 +1,35 @@
+//
+//  Alerts.swift
+//  Floq
+//
+//  Created by Shadrach Mensah on 13/08/2019.
+//  Copyright © 2019 Arun Nagarajan. All rights reserved.
+//
+
+import Foundation
+
+
+struct AlertSystem{
+    static var _TYPE = "type"
+    enum Types {
+    static let USER_JOINED = "jd";
+    static let PHOTO_ADDED = "pa";
+    static let COMMENT_ADDED = "ca";
+    static let PHOTO_LIKED = "pl";
+    
+    }
+    
+    
+    //static func broadcas
+    
+//    static func broadcastAlert(_ userInfo:[AnyHashable:Any]){
+//        guard let type = userInfo[_TYPE] as? String else {return}
+//        switch type {
+//        case Types.COMMENT_ADDED:
+//
+//        default:
+//            <#code#>
+//        }
+//    }
+
+}
